94343a01dc559f4a846bd9f29eb6aeb6899d1c5f
muh-habeeb <muhammadhabeebrahmam@gmail.com>
2025-09-22 14:32:27 +0530

    new: node server by docker

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON PORT : ${PORT}`);
+});
